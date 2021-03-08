d8ba0ad5486472dedd0363a548ef0d8e56f0b61c
williamchoi1984 <66945490+williamchoi1984@users.noreply.github.com>
2021-03-08 22:40:55 +0900

    Update musicbot.py

--- musicbot.py
+++ musicbot.py
@@ -623,7 +623,7 @@ class Music(commands.Cog):
 	@commands.command(name=command[12][0], aliases=command[12][1:])   #도움말
 	async def menu_(self, ctx):
 		command_list = ''
-		command_list += '!인중 : 봇상태가 안좋을 때 쓰세요!\n'     #!
+		command_list += '$인중 : 봇상태가 안좋을 때 쓰세요!\n'     #!
 		command_list += ','.join(command[0]) + '\n'     #!들어가자
 		command_list += ','.join(command[1]) + '\n'     #!나가자
 		command_list += ','.join(command[2]) + ' [검색어] or [url]\n'     #!재생
@@ -646,7 +646,7 @@ class Music(commands.Cog):
 				)
 		await ctx.send( embed=embed, tts=False)
 	################ 음성파일 생성 후 재생 ################ 			
-	@commands.command(name="!인중")
+	@commands.command(name="$인중")
 	async def playText_(self, ctx):
 		#msg = ctx.message.content[len(ctx.invoked_with)+1:]
 		#sayMessage = msg
